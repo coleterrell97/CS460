7d46c5c3aaf4a340ec4fcc55c577d7787a3ef506
Cole Terrell <coleterrell@Coles-MacBook-Pro-2.local>
2020-02-12 18:31:34 -0500

    Added ability to discretize numeric data.

--- Assignment1/syntheticDataParser.py
+++ Assignment1/syntheticDataParser.py
@@ -1,25 +1,41 @@
 #! /usr/local/bin/python3
 import csv
+import numpy as np
 
 
-class syntheticDataRaw:
-    def __init__(self, fileName):
+class syntheticDataSet:
+    def __init__(self, fileName, numFeatures, numBins):
+        self.numFeatures = numFeatures
+        self.numBins = numBins
         self.dataSource = "./data/" + fileName
-        self.data = []
         self.__readCSVFile__()
+        self.__discretizeFeatures__()
 
     def __readCSVFile__(self):
+        dataList = []
         with open(self.dataSource) as dataCSV:
             dataReader = csv.reader(dataCSV)
             for row in dataReader:
-                self.data.append(row)
+                dataList.append(row)
+        self.data=np.array(dataList).astype(float)
 
     def printData(self):
-        for row in self.data:
-            print(row)
+        print(self.data)
+
+    def __discretizeFeatures__(self):
+        max = np.amax(self.data, axis = 0)
+        min = np.amin(self.data, axis = 0)
+        for feature in range(0,self.numFeatures):
+            featureRange= max[feature]-min[feature]
+            binSize = featureRange/self.numBins
+            for dataPoint in self.data:
+                binNumber = 1
+                while dataPoint[feature] > min[feature]:
+                    dataPoint[feature] = dataPoint[feature] - binSize
+                    if(dataPoint[feature] > min[feature]):
+                        binNumber = binNumber + 1
+                dataPoint[feature] = binNumber
 
 
-class syntheticDataFormatted:
-    def __init__(self, rawData)
-test = syntheticDataRaw("synthetic-1.csv")
-test.printData()
+
+test = syntheticDataSet("synthetic-1.csv", 2, 5)
