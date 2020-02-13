4c8385652ede0fb91dacf04afd03c5ee555627dc
Cole Terrell <coleterrell@Coles-MacBook-Pro-2.local>
2020-02-12 22:40:09 -0500

    Added comments to data parser. Began decision tree.

---
+++ Assignment1/decisionTree.py
@@ -0,0 +1,60 @@
+#! /usr/local/bin/python3
+import syntheticDataParser as parser
+import math
+from anytree import Node, RenderTree
+
+def ID3(dataSet, target_attribute, attributes, depth):
+        subTreeRoot = Node(target_attribute, label = -1)
+        labelDistribution = determineClassLabels(dataSet)
+        #base cases
+        if labelDistribution[0] == 0:
+            subTreeRoot.label = 1
+            return subTreeRoot
+        elif labelDistribution[1] == 0:
+            subTreeRoot.label = 0
+            return subTreeRoot
+        elif attributes == [] or depth == 3:
+            if(labelDistribution[0] > labelDistribution[1]):
+                subTreeRoot.label = 1
+                return subTreeRoot
+            elif(labelDistribution[1] > labelDistribution[0]):
+                subTreeRoot.label = 1
+                return subTreeRoot
+            elif(labelDistribution[1] == labelDistribution[0]):
+                subTreeRoot.label = 1
+                return subTreeRoot
+        #end base cases
+        #begin entropy calculation
+        else:
+            target_attribute = findBestSplit(dataSet, labelDistribution, attributes)
+            return subTreeRoot
+
+def determineClassLabels(dataSet):
+    oneLabels = 0
+    zeroLabels = 0
+    labels = [0, 0]
+    for dataPoint in dataSet:
+        if dataPoint[2] == 1:
+            oneLabels = oneLabels + 1
+        elif dataPoint[2] == 0:
+            zeroLabels = zeroLabels + 1
+    labels[0] = zeroLabels
+    labels[1] = oneLabels
+    return labels
+
+def calculateEntropy(dataSet, labelDistribution):
+    entropy = 0
+    for label in labelDistribution:
+        entropy = entropy + (-((label/dataSet.shape[0]) * math.log(label/dataSet.shape[0],2)))
+    return entropy
+
+def findBestSplit(dataSet, labelDistribution, attributes):
+
+    parentEntropy = calculateEntropy(dataSet, labelDistribution)
+    return 0
+
+
+
+trainingData = parser.syntheticDataSet("synthetic-1.csv", 2, 5)
+decisionTree = ID3(trainingData.data, "ROOT", [0,1], 0)
+print(RenderTree(decisionTree))
--- Assignment1/syntheticDataParser.py
+++ Assignment1/syntheticDataParser.py
@@ -3,6 +3,10 @@ import csv
 import numpy as np
 
 
+#syntheticDataSet
+#Description: Contains data and function members for storing and discretizing CSV input data
+#such that it can be used to train and test a decision tree.
+
 class syntheticDataSet:
     def __init__(self, fileName, numFeatures, numBins):
         self.numFeatures = numFeatures
@@ -10,6 +14,8 @@ class syntheticDataSet:
         self.dataSource = "./data/" + fileName
         self.__readCSVFile__()
         self.__discretizeFeatures__()
+    #end function __init__
+
 
     def __readCSVFile__(self):
         dataList = []
@@ -17,10 +23,14 @@ class syntheticDataSet:
             dataReader = csv.reader(dataCSV)
             for row in dataReader:
                 dataList.append(row)
+        #store all data as a multidimensional array of floats
         self.data=np.array(dataList).astype(float)
+    #end function __readCSVFile__
+
 
     def printData(self):
         print(self.data)
+    #end function printData
 
     def __discretizeFeatures__(self):
         max = np.amax(self.data, axis = 0)
@@ -28,6 +38,7 @@ class syntheticDataSet:
         for feature in range(0,self.numFeatures):
             featureRange= max[feature]-min[feature]
             binSize = featureRange/self.numBins
+            #round continuous data to a range of values between 1 and 5
             for dataPoint in self.data:
                 binNumber = 1
                 while dataPoint[feature] > min[feature]:
@@ -35,7 +46,5 @@ class syntheticDataSet:
                     if(dataPoint[feature] > min[feature]):
                         binNumber = binNumber + 1
                 dataPoint[feature] = binNumber
-
-
-
-test = syntheticDataSet("synthetic-1.csv", 2, 5)
+    #end function __discretizeFeatures__
+#End class syntheticDataSet
